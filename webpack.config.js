5d55ece2f0109ce4b91306622f598a11bcac5917
楼教主 <cik520@qq.com>
2017-04-05 23:02:31 +0800

    更新编译脚本

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,21 @@
 const path = require('path');
 const webpack = require('webpack');
 
+// 开发模式
+const isDev = process.env.NODE_ENV === 'development';
+
+// 插件
+const plugins = [];
+
+if (!isDev) {
+  plugins.push(new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false,
+    },
+    sourceMap: false,
+  }));
+}
+
 function resolve(dir) {
   return path.resolve(__dirname, dir);
 }
@@ -14,6 +29,7 @@ module.exports = {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devtool: isDev ? '#cheap-module-eval-source-map' : false, // 开发模式
   module: {
     rules: [
       {
@@ -23,12 +39,5 @@ module.exports = {
       },
     ],
   },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-      },
-      sourceMap: false,
-    }),
-  ],
+  plugins,
 };
